bb59cc7841962fec72410763048dd8a0d8e5531b
Jan Sindler <jansindl3r@gmail.com>
2021-05-05 20:29:37 +0200

    cleaned

--- rotorizer.py
+++ rotorizer.py
@@ -6,17 +6,20 @@ from ufo2ft import compileVariableTTF, compileOTF
 from fontTools.designspaceLib import (
     DesignSpaceDocument,
     SourceDescriptor,
-    AxisDescriptor,
+    AxisDescriptor
 )
 from datetime import datetime
-from copy import deepcopy, copy 
+from copy import deepcopy, copy
+
 
 @property
 def position(self):
     return self.x, self.y
 
+
 setattr(Point, "position", position)
 setattr(Point, "type", Point.segmentType)
+
 
 def font__deepcopy__(self, settings):
     clean = self.__class__()
@@ -36,6 +39,7 @@ def process_glyph(glyph, draw_sides, absolute=False, width=160):
             zip(contour.segments, range(len(contour.segments))),
             key=lambda x: x[0][-1].position[::-1],
         )[1]
+
         contour_reordered = (
             contour.segments[lowest_point:] + contour.segments[:lowest_point]
         )
@@ -51,7 +55,7 @@ def process_glyph(glyph, draw_sides, absolute=False, width=160):
             values = values[-index:] + values[:-index]
         last_value = values[0]
         drawing = contour.__class__()
-        edges = [glyph.width / 2 + width/2, glyph.width / 2 - width/2]
+        edges = [glyph.width / 2 + width / 2, glyph.width / 2 - width / 2]
         if absolute:
             edges = edges[:1] * 2
         if contour.clockwise:
@@ -83,13 +87,17 @@ def process_glyph(glyph, draw_sides, absolute=False, width=160):
         drawings.append(drawing)
     return drawings
 
-import concurrent
 
 def process_font(ufo, draw_sides, absolute=False, width=160):
     font = deepcopy(ufo)
     for glyph in font:
         if len(glyph):
-            drawing = [i for i in process_glyph(glyph, draw_sides, absolute=absolute, width=width)]
+            drawing = [
+                i
+                for i in process_glyph(
+                    glyph, draw_sides, absolute=absolute, width=width
+                )
+            ]
             for contour in [contour for contour in glyph]:
                 glyph.removeContour(contour)
             for contour in drawing:
@@ -102,53 +110,52 @@ def process_font(ufo, draw_sides, absolute=False, width=160):
 def flip(ufo):
     ufo = deepcopy(ufo)
     for glyph in ufo:
-        width_half = glyph.width/2
+        width_half = glyph.width / 2
         for contour in glyph:
             for point in contour:
-                point.x = width_half+(width_half-point.x)
+                point.x = width_half + (width_half - point.x)
     return ufo
-    
+
 
 def align(font, value):
     font = deepcopy(font)
     for glyph in font:
-        width_half = glyph.width/2
+        width_half = glyph.width / 2
         for contour in glyph:
             for point in contour:
-                point.x = width_half+value
+                point.x = width_half + value
     return font
 
+
 def make_designspace():
-    axes = {
-        "rotation": [0, 90, 90.0001, 180, 270, 270.0001, 360]
-    }
+    axes = {"rotation": [0, 90, 90.0001, 180, 270, 270.0001, 360]}
     doc = DesignSpaceDocument()
-    
     axis = AxisDescriptor()
     axis.minimum = 0
     axis.maximum = 360
     axis.default = 0
     axis.name = "rotation"
     axis.tag = "RTTX"
-
     doc.addAxis(axis)
-    for i, font in enumerate([None]*len(axes["rotation"])):
+    for i, font in enumerate([None] * len(axes["rotation"])):
         source = SourceDescriptor()
         source.font = font
         if i == 0:
             source.copyLib = True
             source.copyInfo = True
             source.copyFeatures = True
-        source.location = {"rotation":axes["rotation"][i]}
+        source.location = {"rotation": axes["rotation"][i]}
         doc.addSource(source)
     return doc
 
+
 designspace_template = make_designspace()
+
 
 def designspace(fonts):
     designspace_copy = deepcopy(designspace_template)
     for source, font in zip(designspace_copy.sources, fonts):
-        source.font =  font
+        source.font = font
     return designspace_copy
 
 
@@ -156,17 +163,13 @@ def rotorize(ufo=None, width=80, **kwargs):
     designspaces = []
     if isinstance(width, str):
         width = round(float(width))
-    for glyph in ufo:
-        if len(glyph):
-            glyph.correctContourDirection(segmentLength=5)
-    
     master_0 = process_font(ufo, False, False, width)
     master_90 = process_font(ufo, True, False, width)
     master_90_flipped = flip(master_90)
     master_0_flipped = flip(master_0)
-    master_90_flipped_left = align(master_90_flipped, -width/2)
-    master_90_flipped_right = align(master_90_flipped, width/2)
-    
+    master_90_flipped_left = align(master_90_flipped, -width / 2)
+    master_90_flipped_right = align(master_90_flipped, width / 2)
+
     for i in range(2):
         if i == 0:
             fonts = (
@@ -176,7 +179,7 @@ def rotorize(ufo=None, width=80, **kwargs):
                 master_0_flipped,
                 master_90_flipped,
                 master_90,
-                master_0
+                master_0,
             )
         elif i == 1:
             fonts = (
@@ -186,12 +189,14 @@ def rotorize(ufo=None, width=80, **kwargs):
                 master_0_flipped,
                 master_90_flipped_right,
                 master_90_flipped_left,
-                master_0
+                master_0,
             )
         designspaces.append(designspace(fonts))
     return designspaces
 
-
+if __name__ == "__main__":
+    font = Font("gabion.ufo")
+    process_font(font, True)
 
     # for glyph in ufo:
     #     if len(glyph):
